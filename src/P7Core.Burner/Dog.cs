755365049018925bf8a044cc9ba97f275c9e2590
ghstahl@gmail.com <ghstahl@gmail.com>
2019-01-03 07:55:15 -0800

    More complex Dog class "Heidi"

--- src/P7Core.Burner/Dog.cs
+++ src/P7Core.Burner/Dog.cs
@@ -4,6 +4,31 @@ namespace P7Core.Burner
 {
     public class Dog
     {
-        public string Name => "Heidi";
+        public Dog()
+        {
+
+        }
+
+        public Dog(string name)
+        {
+            Name = name;
+        }
+
+        private string _dog;
+
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_dog))
+                {
+                    _dog = "Heidi";
+                }
+
+                return _dog;
+            }
+            set => _dog = value;
+        }
     }
+
 }
