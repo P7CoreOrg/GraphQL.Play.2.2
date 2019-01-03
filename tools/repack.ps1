0ebd8ad736f6a10e4725a3848cbb041b3a648d3b
ghstahl@gmail.com <ghstahl@gmail.com>
2019-01-02 16:59:40 -0800

    had to convert the pdb using pdb2pdb.exe for ILRepack to properly build the new pdb

--- tools/repack.ps1
+++ tools/repack.ps1
@@ -2,41 +2,53 @@
 
 
 param (
-    [string]$configJson = "config.json",
     [string]$targetDir = "targetDir",
     [string]$rootDir = "rootDir",
     [string]$projectDir = "projectDir"
- )
+)
 
-write-output $configJson
-write-output $targetDir
+ 
+
+
 write-output $rootDir
 write-output $projectDir
 
-$ILRepackJson = $projectDir+"ILRepack.json"
+$ILRepackJson = $projectDir + "ILRepack.json"
 
 $jsonObj = (Get-Content $ILRepackJson) -join "`n" | ConvertFrom-Json
-$outAssembly = $jsonObj.outputAssembly
-$outTarget = "$targetDir$outAssembly"
-$repackTarget = $targetDir+$jsonObj.outputAssembly
 
+$assemblyPDB = $targetDir + $jsonObj.outputAssembly + ".pdb"
+$portablePDB = $targetDir + "portable\" + $jsonObj.outputAssembly + ".pdb"
+$repackTarget = $targetDir + $jsonObj.outputAssembly + ".dll"
 
-$execFile = $rootDir+"tools\ILRepack 2.0.16\tools\ILRepack.exe"
 $listParams = New-Object System.Collections.Generic.List[System.Object]
+$ILRepackCLI = $rootDir + "tools\ILRepack 2.0.16\tools\ILRepack.exe"
+$pdb2PDB = $rootDir + "tools\Pdb2Pdb\tools\Pdb2Pdb.exe"
+
+$listParams.Add("$repackTarget ")
+$listParams.Add("/pdb " + $assemblyPDB)
+#$listParams.Add("/out " + $portablePDB)
+$params = $listParams.ToArray()
+
+write-output "----------PDB2PDB---------------------"
+write-output $pdb2PDB $params
+
+ 
+
+$listParams.Clear();
 $listParams.Add("/lib:$targetDir")
 $listParams.Add("/internalize")
 #$listParams.Add("/ndebug")
-$listParams.Add("/out:"+$repackTarget)
+$listParams.Add("/out:" + $repackTarget)
 
-foreach($assembly in $jsonObj.assembliesToMerge){
-    $listParams.Add($targetDir+$assembly)
+foreach ($assembly in $jsonObj.assembliesToMerge) {
+    $listParams.Add($targetDir + $assembly + ".dll")
 }
 $params = $listParams.ToArray()
 
 # Wait until the started process has finished
-& $execFile $params
-if (-not $?)
-{
+& $ILRepackCLI $params
+if (-not $?) {
     # Show error message
 }
 #Remove-Item $outTarget
