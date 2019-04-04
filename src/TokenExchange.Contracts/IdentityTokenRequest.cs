1132c93e433a34aed8bb6a8f93fc0a73164f7293
ghstahl@gmail.com <ghstahl@gmail.com>
2019-04-04 10:30:37 -0700

    missing

---
+++ src/TokenExchange.Contracts/IdentityTokenRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TokenExchange.Contracts
+{
+    public class IdentityTokenRequest
+    {
+        public string Scope { get; set; }
+        public Dictionary<string, List<string>> ArbitraryClaims { get; set; }
+        public string Subject { get; set; }
+        public int? AccessTokenLifetime { get; set; }
+    }
+}
