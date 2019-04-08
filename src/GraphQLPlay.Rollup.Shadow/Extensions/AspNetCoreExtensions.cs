93257bec895aad3a34004196b1d0faa639f57b30
Herb Stahl <ghstahl@gmail.com>
2019-04-08 12:00:22 -0700

    Preparing to rollup this project as a simple to use nuget

--- src/GraphQLPlay.Rollup.Shadow/Extensions/AspNetCoreExtensions.cs
+++ src/GraphQLPlay.Rollup.Shadow/Extensions/AspNetCoreExtensions.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using IdentityTokenExchangeGraphQL.Extensions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using P7Core.GraphQLCore.Extensions;
+using P7Core.GraphQLCore.Stores;
 using System;
+using TokenExchange.Contracts.Extensions;
+using TokenMintingService.Extensions;
 
-namespace GraphQLPlay.Rollup.Shadow
+namespace GraphQLPlay.Rollup.Extensions
 {
     public static class AspNetCoreExtensions
     {
-        public static IServiceCollection AddGraphQLPlayRollup(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddGraphQLPlayRollup(this IServiceCollection services)
         {
+            services.AddGraphQLCoreTypes();
+            services.AddGraphQLIdentityTokenExchangeTypes();
+            services.AddPrincipalEvaluatorRouter();
+            services.AddInProcTokenMintingService();
+            return services;
+        }
+        public static IServiceCollection AddGraphQLPlayRollupInMemoryServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.TryAddSingleton<IGraphQLFieldAuthority, InMemoryGraphQLFieldAuthority>();
+            services.RegisterGraphQLCoreConfigurationServices(configuration);
+
             return services;
         }
     }
