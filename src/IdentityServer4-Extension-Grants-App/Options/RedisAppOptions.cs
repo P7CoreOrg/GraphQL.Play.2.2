fbe2ac9382a160ad538274cdf680d5864fac5681
Herb Stahl <ghstahl@gmail.com>
2019-05-03 11:40:15 -0700

    removed unused apps.

--- src/IdentityServer4-Extension-Grants-App/Options/RedisAppOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IdentityServer4_Extension_Grants_App.Options
-{
-    public class RedisAppOptions
-    {
-        public bool UseRedis { get; set; }
-        public string RedisConnectionString { get; set; }
-    }
-}
