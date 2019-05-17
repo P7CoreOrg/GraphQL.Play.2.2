e91da92338865ef01d5d9cef6e09fce0e93e9a11
Herb Stahl <ghstahl@gmail.com>
2019-05-17 13:56:46 -0700

    missing

---
+++ src/AppIdentity/Extensions/AppIdentityCreateInputModelExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using AppIdentity.Contracts;
+using AppIdentity.Models;
+using P7Core.Utils;
+
+namespace AppIdentity.Extensions
+{
+    public static class AppIdentityCreateInputModelExtensions
+    {
+        static void ValidateOperation(string exceptionMessage,Func<bool> condition)
+        {
+            if (!condition.Invoke())
+            {
+                throw new GraphQL.ExecutionError(exceptionMessage);
+            }
+        }
+        public static void ValidateConstraints(
+            this AppIdentityCreateInputModel model, 
+            IAppIdentityConfiguration appIdentityConfiguration)
+        {
+            ValidateOperation("subject out of range", () =>
+            {
+                if (string.IsNullOrWhiteSpace(model.Subject)) return true;
+                if (model.Subject.Length > appIdentityConfiguration.MaxSubjectLength) return false;
+                return true;
+               
+            });
+            ValidateOperation("appId out of range", () =>
+            {
+                if (string.IsNullOrWhiteSpace(model.AppId) ||
+                    model.AppId.Length > appIdentityConfiguration.MaxAppIdLength)
+                {
+                    return false;
+                }
+                return true;
+            });
+            ValidateOperation("machineId out of range", () =>
+            {
+                if (string.IsNullOrWhiteSpace(model.MachineId) ||
+                    model.MachineId.Length > appIdentityConfiguration.MaxMachineIdLength)
+                {
+                    return false;
+                }
+                return true;
+            });
+        }
+    }
+}
