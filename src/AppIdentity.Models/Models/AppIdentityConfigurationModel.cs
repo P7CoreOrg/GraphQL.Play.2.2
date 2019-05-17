6bade18db010354c8b2b54538b581f4a10671716
Herb Stahl <ghstahl@gmail.com>
2019-05-17 13:53:56 -0700

    Missing

---
+++ src/AppIdentity.Models/Models/AppIdentityConfigurationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppIdentity.Models
+{
+    public class AppIdentityConfigurationModel
+    {
+        public int MaxSubjectLength { get; set; }
+        public int MaxAppIdLength { get; set; }
+        public int MaxMachineIdLength { get; set; }
+    }
+}
