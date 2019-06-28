91bc3d7c4e403ee10a4df2181c5c674ac37c1c0b
Herb Stahl <ghstahl@gmail.com>
2019-06-28 15:31:19 -0700

    added state

--- src/OIDCWebApp/Controllers/OIDCController.cs
+++ src/OIDCWebApp/Controllers/OIDCController.cs
@@ -13,13 +13,15 @@ namespace OIDC.ReferenceWebClient.Controllers
 {
     public class IdTokenAuthorizationRequest
     {
+       
+
         /*
-         client_id=mvc
-                &redirect_uri=https%3A%2F%2Fp7core.127.0.0.1.xip.io%3A44311%2Fsignin-oidc
-                &response_type=id_token
-                &scope=openid%20profile
-                &response_mode=form_post
-                &nonce=636973229335838266.ZWJhM2U4M2YtYWNiYi00YjZkLTkwMWYtNjRmMjM3MWRiYTk5OWNkNDIzMWUtZmY4OS00YWE0LTk4MGUtMTdiMjYxNmNmZjRk&state=CfDJ8KOz5LEySMhBtqpccMk4UVhA1PvGQQvpqQBUyR-97TDZvaPuNquTLJIUxKMYzF-Ov_HHCnnmcTForzd5RJ4jmLONvcZLY3XCHnrhh9Sc2oR2Lv2HACvPVBMy2oYmmPBtNIoXroQ9WePE_KtPyFw8ntRsHIYMmT5a0fLKGeJcwK3ewoiRHxjKpOr9hXZau9f7CVVqMvtWC2ngWrFsEeh8S0YtRZQFT-7XyjE9dNiyKp_Z-4iBUbbqzVnT4GmEmErZXUjmBhmVsMLz5h9y_F3usRT3lg7LxUNamnJuROnYIqmJzf0fYVJq1mcB5hcUipo2SNcILG3xkUikc84VznSGvD7V_qFjOHVPtOEX02JH9M4ymb3iZtZSE9dDr2RkwTU7StoKgM-x195bBULpwms8weJO-kx5I6UrY_lmWl0SFqYN
+client_id=mvc
+&redirect_uri=https%3A%2F%2Fp7core.127.0.0.1.xip.io%3A44311%2Fsignin-oidc
+&response_type=id_token
+&scope=openid%20profile
+&response_mode=form_post
+&nonce=636973229335838266.ZWJhM2U4M2YtYWNiYi00YjZkLTkwMWYtNjRmMjM3MWRiYTk5OWNkNDIzMWUtZmY4OS00YWE0LTk4MGUtMTdiMjYxNmNmZjRk&state=CfDJ8KOz5LEySMhBtqpccMk4UVhA1PvGQQvpqQBUyR-97TDZvaPuNquTLJIUxKMYzF-Ov_HHCnnmcTForzd5RJ4jmLONvcZLY3XCHnrhh9Sc2oR2Lv2HACvPVBMy2oYmmPBtNIoXroQ9WePE_KtPyFw8ntRsHIYMmT5a0fLKGeJcwK3ewoiRHxjKpOr9hXZau9f7CVVqMvtWC2ngWrFsEeh8S0YtRZQFT-7XyjE9dNiyKp_Z-4iBUbbqzVnT4GmEmErZXUjmBhmVsMLz5h9y_F3usRT3lg7LxUNamnJuROnYIqmJzf0fYVJq1mcB5hcUipo2SNcILG3xkUikc84VznSGvD7V_qFjOHVPtOEX02JH9M4ymb3iZtZSE9dDr2RkwTU7StoKgM-x195bBULpwms8weJO-kx5I6UrY_lmWl0SFqYN
 */
         public string client_id { get; set; }
         public string client_secret { get; set; }
@@ -27,6 +29,8 @@ namespace OIDC.ReferenceWebClient.Controllers
         public string response_type { get; set; }
         public string nonce { get; set; }
         public string response_mode { get; set; }
+        public string state { get; set; }
+        public string scope { get; set; }
     }
     [ApiController]
     [Route("")]
@@ -240,7 +244,9 @@ client_id=mvc
                 nonce = values.Get(OidcConstants.AuthorizeRequest.Nonce),
                 response_mode = values.Get(OidcConstants.AuthorizeRequest.ResponseMode),
                 redirect_uri = values.Get(OidcConstants.AuthorizeRequest.RedirectUri),
-                response_type = values.Get(OidcConstants.AuthorizeRequest.ResponseType)
+                response_type = values.Get(OidcConstants.AuthorizeRequest.ResponseType),
+                state = values.Get(OidcConstants.AuthorizeRequest.State),
+                scope = values.Get(OidcConstants.AuthorizeRequest.Scope)
             };
             this.SetJsonCookie( "idTokenAuthorizationRequest", idTokenAuthorizationRequest, 30);
           
